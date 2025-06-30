413cef66f1f67d3558d1a3c0e0728f90ed54a7ce
AkshitShekhawat <akw17439@gmail.com>
2025-06-30 19:47:50 +0530

    h2 database enabled

--- securitydemo/src/main/java/com/example/securitydemo/SecurityConfig.java
+++ securitydemo/src/main/java/com/example/securitydemo/SecurityConfig.java
@@ -6,7 +6,9 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,11 +23,14 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)
-                requests
-                        .anyRequest()).authenticated());
+                requests.requestMatchers("/h2-console/**").permitAll().anyRequest()).authenticated());
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
        //http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
+        http.headers(headers ->
+                headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        http.csrf(AbstractHttpConfigurer::disable);
+        //http.csrf(csrf -> csrf.disable());
         return (SecurityFilterChain)http.build();
     }
 
