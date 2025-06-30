12260b074d1d2bcd036d49c393c4889a8c023560
AkshitShekhawat <akw17439@gmail.com>
2025-06-30 22:52:42 +0530

    Using Database to Store User Credentials with Spring Security

---
+++ securitydemo/src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+create table users(username varchar_ignorecase(50) not null primary key,password varchar_ignorecase(500) not null,enabled boolean not null);
+create table authorities (username varchar_ignorecase(50) not null,authority varchar_ignorecase(50) not null,constraint fk_authorities_users foreign key(username) references users(username));
+create unique index ix_auth_username on authorities (username,authority);
+
+-- we get this schema from the official source code of spring security
+--  1. search on browser - spring security GitHub and search user.ddl and copy code 
